a92419b949c6e881f268c39d2cdac9ddb676c0aa
ryan <seoingyo@naver.com>
2025-05-30 19:36:19 +0900

    Fix: Update Next.js and package configurations

--- next.config.js
+++ next.config.js
@@ -10,6 +10,13 @@ const nextConfig = {
     ],
     domains: ['images.unsplash.com'],
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /framer-motion/,
+      sideEffects: false
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig 
